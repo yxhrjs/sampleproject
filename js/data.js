784cacd5939a09201c6d5f33b5a561b3c8908638
Macarubbo, C. F <c.f.macarubbo@accenture.com>
2020-04-17 05:41:38 +0800

    partial template added

---
+++ js/data.js
@@ -0,0 +1,60 @@
+{
+	"product": [{
+			"name": "iphone 3",
+			"brand": "apple",
+"description": "iphone 11",
+            "price": "1280",
+            "originalPrice": "2280"
+
+		},
+		{
+			"name": "iphone 4",
+			"brand": "apple",
+"description": "iphone 11",
+			"price": "2280",
+            "originalPrice": "3280"
+        },
+		{
+			"name": "iphone 5",
+			"brand": "apple",
+"description": "iphone 11",
+			"price": "3280",
+            "originalPrice": "4280"
+		},
+		{
+			"name": "iphone 6",
+			"brand": "apple",
+"description": "iphone 11",
+			"price": "4280",
+            "originalPrice": "5280"
+        },
+		{
+			"name": "iphone 7",
+			"brand": "apple",
+"description": "iphone 11",
+			"price": "5280",
+            "originalPrice": "6280"
+        },
+		{
+			"name": "iphone 8",
+			"brand": "apple",
+"description": "iphone 11",
+			"price": "6280",
+            "originalPrice": "7280"
+        },
+        {
+			"name": "iphone 9",
+			"brand": "apple",
+"description": "iphone 11",
+			"price": "7280",
+            "originalPrice": "8280"
+		},
+		{
+			"name": "iphone 11",
+			"brand": "apple",
+" description": "iphone 11",
+			"price": "8280",
+            "originalPrice": "9280"
+		}
+	]
+}
